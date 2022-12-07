474c8de67d3802ca96554707f08eb1913225e9b3
LakayFTW <MilchmannLetsPlay@gmail.com>
2022-12-08 00:14:46 +0100

    completed day1 part2

--- 2022/Day1/day1.py
+++ 2022/Day1/day1.py
@@ -3,8 +3,6 @@ with open("2022/Day1/input.txt") as f:
 
 s = s.strip().split("\n\n")
 
-# print(s[0])
-
 for y in range(len(s)):
     s[y] = s[y].split("\n")
 
@@ -12,12 +10,17 @@ for y in s:
     for x in range(len(y)):
         y[x] = int(y[x])
 
-# print(s)
 test = []
 for z in range(len(s)):
-    # print(s)
     s[z] = (sum(s[z]))
 
 s.sort(reverse=True)
 
 print(s[0])
+
+# Part Two
+sum = 0
+for x in s[:3]:
+    sum += x
+
+print(sum)
